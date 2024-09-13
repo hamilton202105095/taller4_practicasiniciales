796313a9dbee2b2f4b68e4ae19509604779512e8
hamilton202105095 <hamiltonbethancourtzapeta@gmail.com>
2024-09-13 12:41:37 -0600

    errores corregidos para logica

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,8 +4,6 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
-import likeRoutes from "./routes/likes.js";
-import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
 import multer from "multer";
 import cookieParser from "cookie-parser";
@@ -43,8 +41,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
-app.use("/api/likes", likeRoutes);
-app.use("/api/relationships", relationshipRoutes);
 
 app.listen(8800, () => {
   console.log("API working!");
